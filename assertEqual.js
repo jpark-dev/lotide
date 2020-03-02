f4d3c5d3b7a47230df1d942abafb62a3c6dfa12c
Junsung Park <zeipar@gmail.com>
2020-03-02 23:31:16 +0000

    added emoji and texts to check erros and print strings

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: '" + actual + "' !== '" + expected + "'");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("hE", "hE");
+assertEqual(1, 99);
+assertEqual(1, 1);
