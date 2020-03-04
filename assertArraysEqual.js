4519165cf0a8623a63fdce596756d65ab712c30b
Junsung Park <zeipar@gmail.com>
2020-03-04 00:36:06 +0000

    linted

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,16 @@
-const eqArrays = function (arr1, arr2){
+const eqArrays = function(arr1, arr2) {
 
-  if (arr1.length !== arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
-  for (i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(arr1, arr2) {
@@ -20,7 +20,7 @@ const assertArraysEqual = function(arr1, arr2) {
   } else {
     console.log(`🛑🛑🛑 Arrays not equal: ${arr1} !== ${arr2}`);
   }
-}; 
+};
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
