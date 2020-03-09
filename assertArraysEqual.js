3ec0c60bd64fdaaf352b426e8e20e1167961b6aa
Junsung Park <zeipar@gmail.com>
2020-03-09 22:19:59 +0000

    refactoring during pair programming

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,5 @@
 // pair programming with Jas @J-sabharwal
-const eqArrays = function(arr1, arr2) {
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(arr1, arr2) {
