c0de8748497ab3a3bd161c24368425467d9fd1c7
Junsung Park <zeipar@gmail.com>
2020-03-22 06:23:34 +0000

    check of nested objects via recursion is implemented

--- eqObjects.js
+++ eqObjects.js
@@ -1,19 +1,41 @@
-const eqArrays = require('./eqArrays');
-
 const eqObjects = (object1, object2) => {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
+  // loop object
+  for (const key in object1) {
+    // check # keys in both object
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      return false;
+    }
+
+    // if at least one of current element is an array
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      // check length of array
+      if (object1[key].length !== object2[key].length) {
+        return false;
+      }
+      // loop through array and compare values
+      for (const el in object1[key]) {
+        if (object1[key][el] !== object2[key][el]) {
+          return false;
+        }
+      }
+    }
+    // only objects from here (no arrays)
+
+    // if both values are object, recurse 
+    if (typeof object1[key] === 'object' && typeof object2[key] === 'object'){
+      eqObjects(object1[key], object2[key]);
+      
+      // if at least one of values is not an object, compare the values
+    } else {   
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
   }
 
-  for (const key in object1) {
-    if (Array.isArray(object1[key]) !== Array.isArray(object2[key])) {
-      return false;
-    }
-    if (!eqArrays(object1[key], object2[key])) {
-      return false;
-    }
-  }
+  // if no checks, return true
   return true;
+
 };
 
 module.exports = eqObjects;
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -4,8 +4,8 @@ const assert = require('chai').assert;
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+const cd = { c: "1", d: ["2", 3, "4"] };
+const dc = { d: ["2", 3, "4"], c: "1" };
 
 describe('#eqObjectsTest', () => {
   it('should return true for given objects ab, ba', () => {
@@ -20,19 +20,11 @@ describe('#eqObjectsTest', () => {
   it('should return true for given objects { a: { z: 1 }, b: 2 } { a: { z: 1 }, b: 2 }', () => {
     assert.deepEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
   });
+  it('should return true for given objects { a: { z: 1 }, b: 2 } { a: { z: 1 }, b: 2 }', () => {
+    assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  });
   it('should return true for given objects { a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }', () => {
-  assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+    assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
   });
 
 });
-
-// Should update to support nested objects using recursive.
-// currently only compares objects with one object inside.
-// it('should return true for given objects { a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }', () => {
-//   assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
-// })
-// console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)) // => false
-// // console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)) // => false
-
-
-// // console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, m: { d: 9, e: { j: 7, q: 4, f: { p: 1, b: 2 }}, {a: 3}), false)) // => false
